225834076115eae1e109d261d8c8ff50328a928b
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-06-15 14:44:46 +0530

    If else in java

---
+++ src/Conditions.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Conditions {
+    public static void main(String[] args){
+        //Lecture 3: If else Condition
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age > 18){
+            System.out.println("Adult");
+        }else{
+            System.out.println("Not Adult");
+        }
+
+
+
+    }
+}
