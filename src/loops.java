f04711048160802acfc84ade9d6cc80a247ab581
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-06-18 16:29:10 +0530

    For loop in java

---
+++ src/loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    //Loops  are used when you want to repeat some task
+    //For loop, while loop, do while loops are available in java
+    public static void main(String[] args){
+        //for loop to print "Hello world 100 times"
+        //syntax: for(initialization;condition;updation){do something}
+        //if the condition is not defined in that scenario the loop goes in infinite loop
+        for(int counter = 0 ;counter < 100; counter++){
+
+            System.out.println("Hello World");
+
+    }}
+}
