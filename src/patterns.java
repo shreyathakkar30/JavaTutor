6cd7f5e7678631a45d1dd921ce752f5e0d8a80f2
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-06-19 17:20:12 +0530

    Another patterns question solved

--- src/patterns.java
+++ src/patterns.java
@@ -7,16 +7,16 @@ public class patterns {
     //*****
     //*****
     //Will use Nested loops for this
-    int n = 4;
-    int m = 5;
-    outer loop
-    for (int i =1; i <=n; i++){
-        //inner loop
-        for(int j = 1; j<=m; j++){
-            System.out.print("*");
-        }
-        System.out.println();
-    }
+    int n = 5;
+//    int m = 5;
+    //outer loop
+//    for (int i =1; i <=n; i++){
+//        //inner loop
+//        for(int j = 1; j<=m; j++){
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//    }
 
 
     //print the pattern: Hollow Rectangle
@@ -25,18 +25,18 @@ public class patterns {
         //*   *
         //*****
         //outer loop
-        for (int i = 1; i<=n; i++){
-            //inner loop
-            for(int j= 1; j<=m; j++){
-                //cell -> (i, j)
-                if(i == 1 || j == 1 || i == n || j == m){
-                    System.out.print("*");
-                }else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+//        for (int i = 1; i<=n; i++){
+//            //inner loop
+//            for(int j= 1; j<=m; j++){
+//                //cell -> (i, j)
+//                if(i == 1 || j == 1 || i == n || j == m){
+//                    System.out.print("*");
+//                }else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println();
+//        }
 
         //Question: print the pattern
         //*
@@ -46,13 +46,13 @@ public class patterns {
     //here whatever the row number becomes the number of stars
     //if row number is 1 the number of stars will be 1, if row number is 2 the number of stars is 2, and so on
     //outer loop
-        for (int i = 1; i<=n; i++){
-            //inner loop
-            for (int j =1; j<=i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+//        for (int i = 1; i<=n; i++){
+//            //inner loop
+//            for (int j =1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
         //Question: print the pattern
         //****
@@ -60,17 +60,43 @@ public class patterns {
         //**
         //*
         //outer loop
-        for (int i=n; i>=1; i--){
-            //inner loop will remain same as above
-            for (int j=1; j<=i; j++){
-                System.out.print("*");
+//        for (int i=n; i>=1; i--){
+//            //inner loop will remain same as above
+//            for (int j=1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //Question: print the pattern
+        //      *
+        //     **
+        //    ***
+        //   ****
+//        //outer loop
+//        for (int i = 1; i<=n; i++){
+//            //inner loop
+//            for (int j= 1; j<=n-i; j++){
+//                System.out.print(" ");
+//            }
+//            //another inner loop -> star print
+//            for (int j = 1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+        //print the pattern
+        //1
+        //12
+        //123
+        //1234
+        //12345
+        for (int i =1; i<=n; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print(j);
             }
             System.out.println();
         }
-
-
-
-
 
 }
 }
