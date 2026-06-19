5b3e13b809000fbde258becf9bc559ee1f23bf10
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-06-19 17:52:27 +0530

    Another pattern solved

--- src/patterns.java
+++ src/patterns.java
@@ -104,17 +104,49 @@ public class patterns {
         //123
         //12
         //1
+//        for (int i = 1; i<=n; i++){
+//            for (int j = 1; j<=n-i+1; j++){
+//                System.out.print(j);
+//
+//            }
+//
+//            System.out.println();
+//        }
+
+        //print the pattern(FLOYD'S TRIANGLE)
+        //1
+        //2 3
+        //4 5 6
+        //7 8 9 10
+        //11 12 13 14 15
+    int number = 1;
+        //outer loop
         for (int i = 1; i<=n; i++){
-            for (int j = 1; j<=n-i+1; j++){
-                System.out.print(j);
-
-            }
+            //inner loop
             for (int j = 1; j<=i; j++){
-                System.out.print(" ");
-
+                System.out.print(number+ " ");
+                number++;// number = number +1
             }
             System.out.println();
         }
+
+
+
+        // print the pattern
+        //1
+        //  12
+        //   123
+        //    1234
+        //     12345
+//        for (int i = 1; i<=n; i++){
+//            for (int j = 1; j<=i-1; j++){
+//                System.out.print(" ");
+//            }
+//            for (int j = 1; j<=i; j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
 
 
 }
