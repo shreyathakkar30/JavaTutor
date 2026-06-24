c45412aed73e667da66668a75e0e943e2aac230d
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-06-24 20:16:39 +0530

    Arrays Introduction

---
+++ src/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args){
+        //Arrays is a type of nonprimitive data type
+        //Arrays are common concept of java and C
+        //It stores list of the items of the same type
+        //Jab bhi hume same datatype k boht sare variables ko store karana hota hai to hum usse list ki form me store kra dete hai using Array
+
+    }
+}
