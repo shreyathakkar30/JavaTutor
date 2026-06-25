62a55c0eda204186cc0484bca083358311a2ada7
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-06-25 21:44:27 +0530

    Questions related to 2D Arrays covered

--- src/Arrays2D.java
+++ src/Arrays2D.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Arrays2D {
     public static void main(String[] args){
     //2D Array
@@ -11,7 +13,55 @@ public class Arrays2D {
     //syntax: or Declaration:
     //type[][] arrayName = new type[rows][columns];
     //int [][] numbers= new int [][];
+//        Scanner sc = new Scanner(System.in);
+//        int rows= sc.nextInt();
+//        int columns= sc.nextInt();
+//
+//        int [][] numbers = new int[rows][columns];
+//
+//        //input
+//        //outer loop will handle row
+//        for (int i = 0; i< rows; i++){
+//            //inner loop will handle columns
+//            for (int j = 0;j<columns; j++){
+//                numbers[i][j] = sc.nextInt();
+//            }
+//        }
+//    //output
+//        for (int i=0; i<rows; i++){
+//            for (int j =0; j<columns; j++){
+//                System.out.print(numbers[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
 
+        //question: Take a matrix as input from the user. Search for a given number x and print the indices at which it occurs
+        Scanner sc = new Scanner(System.in);
+        int rows= sc.nextInt();
+        int columns= sc.nextInt();
+
+        int [][] numbers = new int[rows][columns];
+
+        //input
+        //outer loop will handle row
+        for (int i = 0; i< rows; i++){
+            //inner loop will handle columns
+            for (int j = 0;j<columns; j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+        for (int i=0; i< rows; i++){
+            for (int j = 0; j<columns; j++){
+                //compare
+                if (numbers[i][j] == x){
+                    System.out.println("Number found at locations ("+ i + "," + j + ")");
+                //here if you write 1 in all the matrix it will return all the locations
+                }
+
+            }
+        }
 
 
     }
