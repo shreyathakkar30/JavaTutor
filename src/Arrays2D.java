114e0fc800985a31bfb050071abcaa46ccef07e8
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-06-25 21:14:04 +0530

    Started with 2D arrays

---
+++ src/Arrays2D.java
@@ -0,0 +1,20 @@
+public class Arrays2D {
+    public static void main(String[] args){
+    //2D Array
+    //   0 1 2 3 4 5
+    //0|-------------|
+    //1|             |
+    //2| 5*5 matrix  |
+    //3|             |
+    //4|             |
+    //5|-------------|
+    //syntax: or Declaration:
+    //type[][] arrayName = new type[rows][columns];
+    //int [][] numbers= new int [][];
+
+
+
+    }
+
+
+}
