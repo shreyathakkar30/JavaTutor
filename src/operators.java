6a5fead0d908dfb264ed4f28d5165173ae3b27b7
Shreya Thakkar <shreyathakkar2004@gmail.com>
2026-07-02 14:14:40 +0530

    Operators in Java

--- src/operators.java
+++ src/operators.java
@@ -23,6 +23,57 @@ public class operators {
         System.out.println(c);
         System.out.println(d);
 
+        //example of pre decrement
+        int aa = 10;
+        int bb = 0;
+
+        bb = --aa;
+        System.out.println(aa);
+        System.out.println(bb);
+
+        //example of post decrement
+        int cc = 10;
+        int dd = 0;
+         dd = cc--;
+        System.out.println(cc);
+        System.out.println(dd);
+
+        //Relational Operators
+        //==, !=, >, <, >=, <=
+        //Logical Operator
+        //Iy checks that the stmt is logically correct or not
+        // &&-> AND: If both stmt true then returns true
+        // ||-> OR: Any of the one stmt true then return true
+        // !-> NOT: Returns reverse if true then returns false if false then return true
+        //Binary Number System Base 2
+        //4 to Binary
+        //101 to Decimal
+        //0 -> 000
+        //1 -> 001
+        //2 -> 010
+        //3 -> 011
+        //4 -> 100
+        //5 -> 101
+        //6 -> 110
+        //7 -> 111
+        //8 -> 1000
+        //9 -> 1001
+        //Octal System Base 8
+        //Hexadecimal Base 16
+        //Bitwise Operators
+        //&: Binary AND
+        //|: Binary OR
+        //^: Binary XOR
+        //~: Binary One's Complement
+        //<<: Binary Left Shift
+        //>>: Binary Right Shift
+
+
+
+
+
+
+
 
 
     }
